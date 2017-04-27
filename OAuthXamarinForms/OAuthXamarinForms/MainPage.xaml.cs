2492f5268a5a290fe7651d9fe00dd30e15f08bed
Supratim Patra <supratimpatra@live.in>
2017-04-27 10:31:32 +0530

    Initial Commit

---
+++ OAuthXamarinForms/OAuthXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Auth;
+
+namespace OAuthXamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Appearing += InitializeOauth;
+        }
+
+        private void InitializeOauth(object sender, EventArgs e)
+        {
+            new OAuth2Authenticator()
+        }
+    }
+}
---
+++ OAuthXamarinForms/OAuthXamarinForms/OAuthConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuthXamarinForms
+{
+    public class OAuthConstants
+    {
+        const string Auth0Domain = "supratim.auth0.com";
+        const string Auth0ClientID = "2BFNQvyhElDyQam3ytScfAgIlrWRixCH";
+        const string Auth0ClientSecret = "tU4uUqO6zDTS3spvX4CJO3hLdkTGqwgNSx-YROPrpjsbGOF5S-Lbee9DJ87QtOZ1";
+        const string Auth0AuthorizationPath = "https://supratim.auth0.com/authorize";
+        const string Auth0TokenRequestPath = "https://supratim.auth0.com/oauth/token";
+        const string Auth0UserInfoEndPoint = "https://supratim.auth0.com/userinfo";
+    }
+}
